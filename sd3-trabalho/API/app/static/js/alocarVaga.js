96d44dcf19a9e47f27407dc5cdfeee4edc5982d7
th.freitas <gthiago_freitas@hotmail.com>
2022-12-08 00:18:19 -0300

    crud para alocar vaga, dentro do estacionamento

---
+++ sd3-trabalho/API/app/static/js/alocarVaga.js
@@ -0,0 +1,13 @@
+function vagaAlocada(){
+    preventDefault()
+    url = "http://192.168.3.143:5000/estacionamento"
+
+    vaga = document.getElementById("vaga")
+    modelo = document.getElementById("modelo")
+    placa = document.getElementById("placa")
+
+    console.log(vaga)
+    console.log(modelo)
+    console.log(placa)
+
+}
